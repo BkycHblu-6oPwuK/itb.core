a7eaf5b26f5aacaf8e8fbdfbfad3320e69240d81
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-07-11 08:09:25 +0600

    update

--- itb.core/lib/Helpers/WebHelper.php
+++ itb.core/lib/Helpers/WebHelper.php
@@ -6,18 +6,15 @@ class WebHelper
 {
     public static function getUuidV4(): string
     {
-        $rquid = bin2hex(random_bytes(16));
-        $rquid = sprintf(
-            '%08s-%04s-%04x-%04x-%12s',
-            substr($rquid, 0, 8),
-            substr($rquid, 8, 4),
-            (hexdec(substr($rquid, 12, 4)) & 0x0fff) | 0x4000,
-            (hexdec(substr($rquid, 16, 4)) & 0x3fff) | 0x8000,
-            substr($rquid, 20, 12)
+        $data = random_bytes(16);
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+        return vsprintf(
+            '%s%s-%s-%s-%s-%s%s%s',
+            str_split(bin2hex($data), 4)
         );
-
-        return $rquid;
     }
+
     public static function generateCode($length = 6): string
     {
         $result = '';
@@ -34,6 +31,7 @@ class WebHelper
     {
         global $APPLICATION;
         $APPLICATION->RestartBuffer();
+        header('Content-Type: application/json');
         echo \Bitrix\Main\Web\Json::encode($result);
         \CMain::FinalActions();
     }
