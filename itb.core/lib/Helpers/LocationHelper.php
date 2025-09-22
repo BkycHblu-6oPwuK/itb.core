c99ac56a2d01a6840f8d79952d5bc9ac436b3879
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-09-22 09:40:07 +0600

    добавлен find метод для поиска через компонент location

--- itb.core/lib/Helpers/LocationHelper.php
+++ itb.core/lib/Helpers/LocationHelper.php
@@ -4,16 +4,21 @@ namespace Itb\Core\Helpers;
 
 use Bitrix\Main\Loader;
 
+Loader::IncludeModule("sale");
+
 class LocationHelper
 {
     private function __construct() {}
     
-    public static function get($filter = [], $select = ['*'])
+    /**
+     * Поиск по CSaleLocation
+     */
+    public static function get($filter = [], $select = ['*'], bool $returnFirst = false)
     {
-        if (!Loader::IncludeModule("sale")) {
-            return [];
-        }
         $locations = [];
+        if ($returnFirst) {
+            $filter['nTopCount'] = 1;
+        }
         $dbLocationList = \CSaleLocation::GetList(
             ["SORT" => "ASC"],
             $filter,
@@ -22,6 +27,10 @@ class LocationHelper
             $select
         );
 
+        if ($returnFirst) {
+            return $dbLocationList->Fetch() ?: [];
+        }
+
         while ($arLocation = $dbLocationList->Fetch()) {
             $locations[] = $arLocation;
         }
@@ -29,18 +38,14 @@ class LocationHelper
         return $locations;
     }
 
-    public static function getCityNameByCityCode(string $cityCode) : string
+    public static function getCityNameByCityCode(string $cityCode): string
     {
         $locationInfo = '';
-
-        if (!Loader::IncludeModule("sale")) {
-            return $locationInfo;
-        }
 
         $filter = ["LID" => LANGUAGE_ID, "CODE" => $cityCode];
         $arLocation = static::get($filter, ["CITY_NAME"]);
 
-        if(!empty($arLocation)){
+        if (!empty($arLocation)) {
             $locationInfo = array_shift($arLocation)['CITY_NAME'];
         }
 
@@ -54,26 +59,49 @@ class LocationHelper
 
     public static function getAllCities()
     {
-        return static::get(['CITY_LID' => 'ru'],["CITY_NAME",'ID']);
+        return static::get(['CITY_LID' => 'ru'], ["CITY_NAME", 'ID']);
     }
 
     public static function getLocationInfoByCityName($cityName)
     {
         $locationInfo = [];
 
-        if (!Loader::IncludeModule("sale")) {
-            return $locationInfo;
-        }
-
         $filter = ["LID" => LANGUAGE_ID, "CITY_NAME" => $cityName];
         $arLocation = static::get($filter, ["ID", "CITY_ID", "REGION_ID", "COUNTRY_ID"]);
-        foreach($arLocation as $location){
+        foreach ($arLocation as $location) {
             $locationInfo[] = $location["ID"];
-            $locationInfo[] = $location["CITY_ID"]; 
+            $locationInfo[] = $location["CITY_ID"];
             $locationInfo[] = $location["REGION_ID"];
-            $locationInfo[] = $location["COUNTRY_ID"]; 
+            $locationInfo[] = $location["COUNTRY_ID"];
         }
 
         return array_unique($locationInfo);
+    }
+
+    /**
+     * поиск через компонент sale.location.selector.search
+     */
+    public static function find(string $query, int $pageSize = 50, $page = 0)
+    {
+        \CBitrixComponent::includeComponentClass('bitrix:sale.location.selector.search');
+        $parameters = [
+            'select' => ['CODE', 'TYPE_ID', 'VALUE' => 'ID', 'DISPLAY' => 'NAME.NAME'],
+            'additionals' => ['PATH'],
+            'filter' => ['=PHRASE' => $query, '=NAME.LANGUAGE_ID' => LANGUAGE_ID, '=SITE_ID' => SITE_ID],
+            'PAGE_SIZE' => $pageSize,
+            'PAGE' => $page,
+        ];
+        $data = \CBitrixLocationSelectorSearchComponent::processSearchRequestV2($parameters);
+        $pathItems = $data['ETC']['PATH_ITEMS'];
+        foreach($data['ITEMS'] as &$item) {
+            foreach($item['PATH'] as $keyPath => &$path) {
+                if($pathItems[$path]) {
+                    $path = $pathItems[$path];
+                } else {
+                    unset($item['PATH'][$keyPath]);
+                }
+            }
+        }
+        return $data['ITEMS'];
     }
 }
