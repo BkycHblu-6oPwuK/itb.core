8c3db09521e846974dd159c49511dfee2387965e
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-09-23 14:48:06 +0600

    helper upd

--- itb.core/lib/Helpers/SaleHelper.php
+++ itb.core/lib/Helpers/SaleHelper.php
@@ -7,7 +7,7 @@ use Bitrix\Sale\Internals\StatusTable;
 class SaleHelper
 {
     private function __construct() {}
-    
+
     public static function getStatuses(): array
     {
         return StatusTable::query()->setSelect(['ID', 'NAME' => 'STATUS_LANG.NAME'])->where('STATUS_LANG.LID', \Bitrix\Main\Localization\Loc::getCurrentLang() ?: 'ru')->setCacheTtl(86400)->cacheJoins(true)->fetchAll();
@@ -21,5 +21,14 @@ class SaleHelper
     public static function getActiveDelivery()
     {
         return \Bitrix\Sale\Delivery\Services\Manager::getActiveList();
+    }
+
+    public static function getExtraServiceByCode(string $deliveryCode, string $serviceCode)
+    {
+        $result = \Bitrix\Sale\Delivery\ExtraServices\Table::query()->setSelect(['*'])->where('CODE', $serviceCode)->registerRuntimeField('', new Reference('D', \Bitrix\Sale\Delivery\Services\Table::class, ['=this.DELIVERY_ID' => 'ref.ID']))->where('D.CODE', $deliveryCode)->setCacheTtl(360000)->cacheJoins(true)->exec();
+        if ($service = $result->fetch()) {
+            return (new \Bitrix\Sale\Delivery\ExtraServices\Manager([$service]))->getItemByCode($serviceCode);
+        }
+        return null;
     }
 }
