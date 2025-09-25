7aac0008d376f38fd3b53c9d5ab57df0bbd1069b
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-09-25 14:42:46 +0600

    add function

--- itb.core/lib/functions.php
+++ itb.core/lib/functions.php
@@ -1,1 +1,19 @@
 <?php
+if (!function_exists('firstNotEmpty')) {
+    /**
+     * Возвращает первое непустое значение или значение по умолчанию
+     *
+     * @param mixed $default
+     * @param mixed ...$values
+     * @return mixed
+     */
+    function firstNotEmpty(mixed $default, ...$values): mixed 
+    {
+        foreach ($values as $value) {
+            if (!empty($value)) {
+                return $value;
+            }
+        }
+        return $default;
+    }
+}
