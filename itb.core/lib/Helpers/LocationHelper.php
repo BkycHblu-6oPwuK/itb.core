9c8ff827eb1fa0f09f70252283704bf3c378b647
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2025-09-26 11:22:03 +0600

    update location helper

--- itb.core/lib/Helpers/LocationHelper.php
+++ itb.core/lib/Helpers/LocationHelper.php
@@ -3,105 +3,195 @@
 namespace Itb\Core\Helpers;
 
 use Bitrix\Main\Loader;
+use Bitrix\Main\ORM\Fields\Relations\Reference;
+use Bitrix\Sale\Location\LocationTable;
+use Bitrix\Sale\Location\Name\LocationTable as LocationNameTable;
 
-Loader::IncludeModule("sale");
+Loader::includeModule("sale");
 
 class LocationHelper
 {
     private function __construct() {}
     
     /**
-     * Поиск по CSaleLocation
+     * Универсальный метод выборки из LocationTable c NAME
      */
-    public static function get($filter = [], $select = ['*'], bool $returnFirst = false)
-    {
-        $locations = [];
-        if ($returnFirst) {
-            $filter['nTopCount'] = 1;
+    public static function get(
+        array $filter = ['=NAME.LANGUAGE_ID' => LANGUAGE_ID],
+        array $select = ['*', 'LOCATION_NAME_' => 'NAME'],
+        bool $returnFirst = false,
+        int $cacheTtl = 0
+    ): array {
+        $params = [
+            'filter'  => $filter,
+            'select'  => $select,
+            'order'   => ['SORT' => 'ASC'],
+            'runtime' => [
+                new Reference(
+                    'NAME',
+                    LocationNameTable::class,
+                    ['=this.ID' => 'ref.LOCATION_ID'],
+                    ['join_type' => 'inner']
+                ),
+            ],
+        ];
+
+        if ($cacheTtl > 0) {
+            $params['cache'] = [
+                'ttl'         => $cacheTtl,
+                'cache_joins' => true,
+            ];
         }
-        $dbLocationList = \CSaleLocation::GetList(
-            ["SORT" => "ASC"],
-            $filter,
-            false,
-            false,
-            $select
+
+        if ($returnFirst) {
+            $params['limit'] = 1;
+        }
+
+        $res = LocationTable::getList($params);
+
+        if ($returnFirst) {
+            return $res->fetch() ?: [];
+        }
+
+        return $res->fetchAll();
+    }
+
+    public static function getLocationByCityCode(string $cityCode, int $cacheTtl = 0): array
+    {
+        return static::get(
+            filter: [
+                '=CODE'             => $cityCode,
+                '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+            ],
+            returnFirst: true,
+            cacheTtl: $cacheTtl
+        );
+    }
+
+    public static function getAllLocations(int $cacheTtl = 0): array
+    {
+        return static::get(cacheTtl: $cacheTtl);
+    }
+
+    public static function getAllCities($select = ['ID', 'CITY_NAME' => 'NAME.NAME'], int $cacheTtl = 0): array
+    {
+        return static::get(
+            filter: [
+                '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+                '=TYPE.CODE'        => 'CITY',
+            ],
+            select: $select,
+            cacheTtl: $cacheTtl
+        );
+    }
+
+    public static function getLocationByCityName(string $cityName, int $cacheTtl = 0): array
+    {
+        return static::get(
+            filter: [
+                '=NAME.NAME'        => $cityName,
+                '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+            ],
+            returnFirst: true,
+            cacheTtl: $cacheTtl
+        );
+    }
+
+    public static function getNearestCityByLocationCode(string $locationCode, int $cacheTtl = 0): array
+    {
+        $location = static::get(
+            filter: [
+                '=CODE'             => $locationCode,
+                '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+            ],
+            returnFirst: true,
+            cacheTtl: $cacheTtl
         );
 
-        if ($returnFirst) {
-            return $dbLocationList->Fetch() ?: [];
+        if (!$location) {
+            return [];
+        }
+        toFile(['$location' => $location]);
+        if (!empty($location['CITY_ID']) && $location['CITY_ID'] == $location['ID']) {
+            return $location;
         }
 
-        while ($arLocation = $dbLocationList->Fetch()) {
-            $locations[] = $arLocation;
+        if (!empty($location['CITY_ID'])) {
+            return static::get(
+                filter: [
+                    '=ID'               => $location['CITY_ID'],
+                    '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+                ],
+                returnFirst: true,
+                cacheTtl: $cacheTtl
+            );
         }
 
-        return $locations;
-    }
+        $parentId = $location['PARENT_ID'] ?? null;
+        while ($parentId) {
+            $parent = static::get(
+                filter: [
+                    '=ID'               => $parentId,
+                    '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+                ],
+                returnFirst: true,
+                cacheTtl: $cacheTtl
+            );
 
-    public static function getCityNameByCityCode(string $cityCode): string
-    {
-        $locationInfo = '';
+            if (!$parent) {
+                break;
+            }
 
-        $filter = ["LID" => LANGUAGE_ID, "CODE" => $cityCode];
-        $arLocation = static::get($filter, ["CITY_NAME"]);
+            if (!empty($parent['CITY_ID'])) {
+                return $parent['CITY_ID'] == $parent['ID']
+                    ? $parent
+                    : static::get(
+                        filter: [
+                            '=ID'               => $parent['CITY_ID'],
+                            '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+                        ],
+                        returnFirst: true,
+                        cacheTtl: $cacheTtl
+                    );
+            }
 
-        if (!empty($arLocation)) {
-            $locationInfo = array_shift($arLocation)['CITY_NAME'];
+            $parentId = $parent['PARENT_ID'] ?? null;
         }
 
-        return $locationInfo;
-    }
-
-    public static function getAllLocations()
-    {
-        return static::get();
-    }
-
-    public static function getAllCities()
-    {
-        return static::get(['CITY_LID' => 'ru'], ["CITY_NAME", 'ID']);
-    }
-
-    public static function getLocationInfoByCityName($cityName)
-    {
-        $locationInfo = [];
-
-        $filter = ["LID" => LANGUAGE_ID, "CITY_NAME" => $cityName];
-        $arLocation = static::get($filter, ["ID", "CITY_ID", "REGION_ID", "COUNTRY_ID"]);
-        foreach ($arLocation as $location) {
-            $locationInfo[] = $location["ID"];
-            $locationInfo[] = $location["CITY_ID"];
-            $locationInfo[] = $location["REGION_ID"];
-            $locationInfo[] = $location["COUNTRY_ID"];
-        }
-
-        return array_unique($locationInfo);
+        return [];
     }
 
     /**
      * поиск через компонент sale.location.selector.search
      */
-    public static function find(string $query, int $pageSize = 50, $page = 0)
+    public static function find(string $query, int $pageSize = 50, $page = 0): array
     {
         \CBitrixComponent::includeComponentClass('bitrix:sale.location.selector.search');
         $parameters = [
-            'select' => ['CODE', 'TYPE_ID', 'VALUE' => 'ID', 'DISPLAY' => 'NAME.NAME'],
+            'select'      => ['CODE', 'TYPE_ID', 'VALUE' => 'ID', 'DISPLAY' => 'NAME.NAME'],
             'additionals' => ['PATH'],
-            'filter' => ['=PHRASE' => $query, '=NAME.LANGUAGE_ID' => LANGUAGE_ID, '=SITE_ID' => SITE_ID],
+            'filter'      => [
+                '=PHRASE'           => $query,
+                '=NAME.LANGUAGE_ID' => LANGUAGE_ID,
+                '=SITE_ID'          => SITE_ID,
+            ],
             'PAGE_SIZE' => $pageSize,
-            'PAGE' => $page,
+            'PAGE'      => $page,
         ];
+
         $data = \CBitrixLocationSelectorSearchComponent::processSearchRequestV2($parameters);
         $pathItems = $data['ETC']['PATH_ITEMS'];
-        foreach($data['ITEMS'] as &$item) {
-            foreach($item['PATH'] as $keyPath => &$path) {
-                if($pathItems[$path]) {
+
+        foreach ($data['ITEMS'] as &$item) {
+            foreach ($item['PATH'] as $keyPath => &$path) {
+                if ($pathItems[$path]) {
                     $path = $pathItems[$path];
                 } else {
                     unset($item['PATH'][$keyPath]);
                 }
             }
         }
+
         return $data['ITEMS'];
     }
 }
