83932052918699d4cb12cc912ec5499796b2edb6
Alexandr.Belotsitsko <sanya_belyy_0202@mail.ru>
2025-09-27 10:57:50 +0600

    example functions

--- itb.core/lib/functions.php
+++ itb.core/lib/functions.php
@@ -7,7 +7,7 @@ if (!function_exists('firstNotEmpty')) {
      * @param mixed ...$values
      * @return mixed
      */
-    function firstNotEmpty(mixed $default, ...$values): mixed 
+    function firstNotEmpty(mixed $default, ...$values): mixed
     {
         foreach ($values as $value) {
             if (!empty($value)) {
@@ -15,5 +15,53 @@ if (!function_exists('firstNotEmpty')) {
             }
         }
         return $default;
+    }
+}
+if (!function_exists('toFile')) {
+    function toFile(mixed $data, $path = 'local/logs/1.log'): void
+    {
+        if (!is_array($data)) {
+            $data = [$data];
+        }
+        (new \Itb\Core\Logger\FileLogger($_SERVER['DOCUMENT_ROOT'] . "/{$path}"))->info('', $data);
+    }
+}
+if (!function_exists('isLighthouse')) {
+    function isLighthouse(): bool
+    {
+        return (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome-Lighthouse') !== false);
+    }
+}
+if (!function_exists('isLettersUppercase')) {
+    /**
+     * Проверяет являются ли символы строки заглавными, по умолчанию игнорирует цифры и прочие символы
+     */
+    function isLettersUppercase(string $str, bool $ignoreNonLetters = true): bool
+    {
+        if ($ignoreNonLetters) {
+            $letters = preg_replace('/[^A-Za-zА-Яа-яЁё]/u', '', $str);
+            return $letters !== '' && mb_strtoupper($letters) === $letters;
+        } else {
+            return preg_match('/^[A-ZА-ЯЁ]+$/u', $str);
+        }
+    }
+}
+if (!function_exists('containsOnlyLetters')) {
+    /**
+     * Проверяет является ли символы в строке только буквами
+     */
+    function containsOnlyLetters(string $str): bool
+    {
+        $str = trim($str);
+        return $str !== '' && preg_match('/^[A-Za-zА-Яа-яЁё\s]+$/u', $str) === 1;
+    }
+}
+if (!function_exists('isImport')) {
+    /**
+     * Обмен с 1с или нет
+     */
+    function isImport(): bool
+    {
+        return $_REQUEST['mode'] == 'import';
     }
 }
